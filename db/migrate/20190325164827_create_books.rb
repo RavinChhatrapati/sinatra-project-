264550a22729a95075aef18a94a88be59dd14441
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-28 23:22:47 +0000

    put in key code for encrypted password

--- db/migrate/20190325164827_create_books.rb
+++ db/migrate/20190325164827_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration
     create_table :books do |t|
       t.string :title
       t.string :author
-      t.integer :user_id
+      t.integer :user_id  # foreign key
 
       t.timestamps null: false
     end
